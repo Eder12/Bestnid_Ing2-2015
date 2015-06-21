ede330872c7432791df6950d1cae10a7d2b33dd6
Eder12 <Eder_lp@hotmail.com>
2015-06-21 17:39:23 -0300

    autorización
    mas otras cositas

--- Bestnid/misubasta.php
+++ Bestnid/misubasta.php
@@ -1,6 +1,51 @@
-<?php require_once('Connections/best.php'); ?><?php 
+<?php 
 error_reporting(E_STRICT);
 require_once('Connections/best.php'); 
+?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "";
+$MM_donotCheckaccess = "true";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && true) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "system/accesoDenegado.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($QUERY_STRING) && strlen($QUERY_STRING) > 0) 
+  $MM_referrer .= "?" . $QUERY_STRING;
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
 ?>
 <?php
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
@@ -56,8 +101,12 @@ if (isset($_GET['pageNum_subastaver'])) {
 }
 $startRow_subastaver = $pageNum_subastaver * $maxRows_subastaver;
 
+$colname_subastaver = "-1";
+if (isset($_GET['idUsuarios'])) {
+  $colname_subastaver = (get_magic_quotes_gpc()) ? $_GET['idUsuarios'] : addslashes($_GET['idUsuarios']);
+}
 mysql_select_db($database_best, $best);
-$query_subastaver = "SELECT * FROM subastas INNER JOIN usuarios ON subastas.idUsuarios = usuarios.idUsuarios WHERE usuarios.Usuario = colname ORDER BY subastas.Titulo ASC ";
+$query_subastaver = sprintf("SELECT * FROM subastas WHERE idUsuarios = %s ORDER BY Titulo ASC", $colname_subastaver);
 $query_limit_subastaver = sprintf("%s LIMIT %d, %d", $query_subastaver, $startRow_subastaver, $maxRows_subastaver);
 $subastaver = mysql_query($query_limit_subastaver, $best) or die(mysql_error());
 $row_subastaver = mysql_fetch_assoc($subastaver);
